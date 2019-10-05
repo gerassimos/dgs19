73f90c675277c95cabbac899f7d811b2e37c180e
gerassimos <gerassimos@gmail.com>
2019-10-05 13:29:50 +0300

    iot-c collector add docker image build

---
+++ resources/iot-c/common/src/main/java/BuildDependencies.java
@@ -0,0 +1,9 @@
+public class BuildDependencies {
+
+  public static void main(String[] args) {
+    //NOTE
+    // This is a dummy main class used at docker build time
+    // just to download all the Dependencies (jars)
+    // The goal is to have a Docker image with all the Dependencies downloaded in the local cache
+  }
+}
