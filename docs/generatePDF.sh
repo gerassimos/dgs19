c17aec0aa564c7f053a3ea03f4bf741489045d6e
gerassimos <gerassimos@gmail.com>
2019-10-17 18:21:07 +0300

    generatePDF

--- docs/generatePDF.sh
+++ docs/generatePDF.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# start a web server that is used to generate(serve) the html pages  
+docker run -d --name dgst19 -p 8069:80 -v $(pwd):/usr/local/apache2/htdocs/docs httpd:2.4
+
 rm -rfv pdf
 mkdir pdf
 chmod 777 pdf
@@ -9,7 +12,7 @@ for md_file in $(ls D_*.md); do
   html_file_name=$(echo web_"${md_file}" | sed 's/.md$/.html/')
   pdf_file_name=$(echo "${md_file}" | sed 's/.md$/.pdf/')
   docker run --rm -it --privileged --net=host -v `pwd`:/slides astefanutti/decktape:2.9.2  --size 1024x769 remark \
-  http://localhost/docs/${html_file_name} pdf/${pdf_file_name}
+  http://localhost:8069/docs/${html_file_name} pdf/${pdf_file_name}
 done
 
 
