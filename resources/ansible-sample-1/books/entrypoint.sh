2a394a689adf67e1bfb03468895369ebd3daa6c2
gerassimos <gerassimos@gmail.com>
2019-12-31 17:33:34 +0200

    ansible sample

---
+++ resources/ansible-sample-1/books/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#echo "$VAULT_PASS" > ~/.vault_pass.txt
+#mkdir ~/.ssh
+#ansible-vault --vault-password-file ~/.vault_pass.txt view ansible/ssh_key.txt.secret > ~/.ssh/id_rsa
+#chmod 0600 ~/.ssh/id_rsa
+#
+#ansible-playbook -e "build_sha=$GITHUB_SHA" --vault-password-file ~/.vault_pass.txt -i ansible/hosts ansible/deploy.yml
+
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+export FAPI_HOSTS="devservers"
+ansible-playbook --vault-password-file vault-pw.txt -i inventory.ini \
+ -e FAPI_HOSTS=$FAPI_HOSTS \
+ playbook.yml
