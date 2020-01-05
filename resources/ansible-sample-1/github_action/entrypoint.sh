bd84d914d074274e6378be6d75cae9da0269644d
gerassimos <gerassimos@gmail.com>
2020-01-05 16:37:14 +0200

    ansible integration

--- resources/ansible-sample-1/github_action/entrypoint.sh
+++ resources/ansible-sample-1/github_action/entrypoint.sh
@@ -9,7 +9,17 @@ chmod 0600 ~/.ssh/id_rsa
 echo "GITHUB_SHA $GITHUB_SHA"
 echo "my key $(cat ~/.ssh/id_rsa)"
 echo "id $(id)"
+echo "pwd $(pwd)"
+echo "home $HOME"
+echo "ls -la ~/"
+echo "$(ls -la ~/)"
+
+echo "ls -la ~/.ssh"
+echo "$(ls -la ~/.ssh)"
+ssh ec2-user@54.198.46.177 "pwd"
 ping -c3 54.198.46.177
+ping -c3 8.8.8.8
+ping -c3 srv1.gm76-dev.com
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
@@ -17,3 +27,4 @@ ansible-playbook --vault-password-file ~/vault-pw.txt \
  -i $INVENTORY_PATH \
  -e FAPI_HOSTS=$FAPI_HOSTS \
  $PLAYBOOK_PATH
+
