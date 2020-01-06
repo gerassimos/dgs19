d7d7251e032850faa9fc13ffbb7dd83ce727c9a8
gerassimos <gerassimos@gmail.com>
2020-01-06 13:16:52 +0200

    fix ansible host check

--- resources/ansible-sample-1/github_action/entrypoint.sh
+++ resources/ansible-sample-1/github_action/entrypoint.sh
@@ -7,21 +7,6 @@ mv ~/id_rsa ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 
 echo "GITHUB_SHA $GITHUB_SHA"
-echo "my key $(cat ~/.ssh/id_rsa)"
-echo "id $(id)"
-echo "pwd $(pwd)"
-echo "home $HOME"
-echo "ls -la ~/"
-echo "$(ls -la ~/)"
-
-echo "ls -la ~/.ssh"
-echo "$(ls -la ~/.ssh)"
-ssh ec2-user@54.198.46.177 "pwd"
-ping -c3 54.198.46.177
-ping -c3 8.8.8.8
-ping -c3 srv1.gm76-dev.com
-
-# export ANSIBLE_HOST_KEY_CHECKING=False
 
 ansible-playbook --vault-password-file ~/vault-pw.txt \
  -i $INVENTORY_PATH \
