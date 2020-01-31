89d6730657dcd11648ff59c94e4fa7a526412e65
EC2 Default User <ec2-user@ip-172-31-45-92.ec2.internal>
2020-01-31 12:46:11 +0000

    [iot-c] add traefix example (2)

--- resources/iot-c/deploy_stack.sh
+++ resources/iot-c/deploy_stack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker network create --driver=overlay traefik-public
-#docker stack deploy -c docker-compose-rp-https-le.yml
-docker stack deploy -c docker-compose-rp-http.yml
+docker stack deploy -c docker-compose-rp-https-le.yml iot-c
+#docker stack deploy -c docker-compose-rp-http.yml iot-c
 
