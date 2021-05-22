79a2a739edd3c36af32bf67db7b6d29da24833d9
gerassimos <gerassimos@gmail.com>
2021-05-22 18:58:41 +0300

    jsim implement web server endpoints

--- resources/jsim/README.md
+++ resources/jsim/README.md
@@ -4,11 +4,21 @@
 
 ## Overview
  - This is a simple java web application (single java file) that can be used to simulate cpu/memory loads on a system
+ - By default the web server is listening on port 4567 (not configurable yet)
+ - So for example after the application is started use `http://localhost:4567/details` to display details about the request
  
 ## Endpoints
-TODO
+    GET "/"             =>  Display the server hosname and client IP
+    GET "/cpu"          => Generare CPU load by running Fibonacci sequence
+    GET "/ram"          => Generare Memory load and display memory info
+    GET "/ram/info      => Display memory info
+    GET "/ram/clean     => Clean Memory - Undo "Memory load" 
+    GET "/health        => Display Healthy state http 200 => Healthy!  -- http 401 Not healthy
+    GET "/health/flip   => Flip the healthy state (Healthy=>Not healthy=>Healthy)
+    GET "/details       => Display details about the request
 
 ## How to deploy
- - With Docker: `docker run -p 5000:5000 dgs19/jsim:latest` 
+ - TODO - Unzip the distribution file jsim.zip and run the bin/jsim.bat  *jre 11 is required*
+ - TODO - With Docker: `docker run -p 4567:4567 dgs19/jsim:latest` 
     
 
--- resources/jsim/src/main/java/dgs19/Main.java
+++ resources/jsim/src/main/java/dgs19/Main.java
@@ -1,12 +1,139 @@
 package dgs19;
 
-import org.joda.time.LocalTime;
+import spark.Request;
+import spark.Response;
+
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import static spark.Spark.get;
+import static spark.Spark.threadPool;
 
 public class Main {
+
+  private static List<String> myBigList = new ArrayList<>();
+  private static Boolean isHealthy = true;
+
   public static void main(String[] args) {
-    LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time is: " + currentTime);
-    Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
+
+    int maxThreads = 32;
+    int minThreads = 2;
+    int timeOutMillis = 30000;
+    threadPool(maxThreads, minThreads, timeOutMillis);
+
+    get("/", Main::hello);
+    get("/cpu", Main::cpu);
+    get("/ram", Main::ram);
+    get("/ram/info", Main::ramInfo);
+    get("/ram/clean", Main::ramClean);
+    get("/health", Main::health);
+    get("/health/flip", Main::flipHealth);
+    get("/details", Main::details);
   }
+
+  private static String hello(Request request, Response response) throws UnknownHostException {
+    StringBuilder sb = new StringBuilder();
+
+    String myHostname = InetAddress.getLocalHost().getHostName();
+    sb.append("Hello World By: ").append(myHostname).append("<br/>");
+    String sourceIP = request.ip();
+    sb.append("Receive Request From: ").append(sourceIP).append("<br/>");
+    return sb.toString();
+  }
+
+
+  private static String cpu(Request request, Response response) {
+    long currentTime = System.currentTimeMillis();
+    BigInteger computation = fib(10000);
+    return "fib function took " + (System.currentTimeMillis() - currentTime) + " milliseconds";
+  }
+
+
+  private static String ram(Request request, Response response) {
+    try {
+      String myBigString = new String(new char[10000000]);
+      myBigList.add(myBigString);
+      return ramInfo(request, response);
+    } catch (OutOfMemoryError e){
+      return "OutOfMemory Exception!";
+    }
+  }
+
+  private static String ramInfo(Request request, Response response) {
+    return getRamInfo();
+  }
+
+  private static String ramClean(Request request, Response response) {
+    myBigList.clear();
+    return ramInfo(request, response);
+  }
+
+
+  private static String health(Request request, Response response) {
+    if (isHealthy){
+      return "Healthy!";
+    } else {
+      response.status(401);
+      return "Not healthy";
+    }
+  }
+
+  private static String flipHealth(Request request, Response response) {
+    isHealthy = !isHealthy;
+    return health(request, response);
+  }
+
+
+  private static String details(Request request, Response response) throws UnknownHostException {
+    StringBuilder sb = new StringBuilder();
+    String myHostname = InetAddress.getLocalHost().getHostName();
+    sb.append("Hello World By: ").append(myHostname).append("<br/>");
+    String sourceIP = request.ip();
+    sb.append("Receive Request From: ").append(sourceIP).append("<br/>");
+    sb.append("Request Headers are: ").append("<br/>");
+    request.headers().forEach(header -> sb.append(header).append(": ").append(request.headers(header)).append("<br/>"));
+    return sb.toString();
+  }
+
+  // Fibonacci sequence to perform some long computations
+  private static BigInteger fib(long nth){
+    nth = nth - 1;
+    long count = 0;
+    BigInteger first = BigInteger.ZERO;
+    BigInteger second = BigInteger.ONE;
+
+    BigInteger third = null;
+    while(count < nth){
+      third = new BigInteger(first.add(second).toString());
+      first = new BigInteger(second.toString());
+      second = new BigInteger(third.toString());
+      count++;
+    }
+
+    return third;
+  }
+
+
+  // get some RAM statistics
+  private static String getRamInfo() {
+
+    Runtime runtime = Runtime.getRuntime();
+    NumberFormat format = NumberFormat.getInstance();
+
+    StringBuilder sb = new StringBuilder();
+    long maxMemory = runtime.maxMemory();
+    long allocatedMemory = runtime.totalMemory();
+    long freeMemory = runtime.freeMemory();
+
+    sb.append("free memory: ").append(format.format(freeMemory / 1024)).append("<br/>");
+    sb.append("allocated memory: ").append(format.format(allocatedMemory / 1024)).append("<br/>");
+    sb.append("max memory: ").append(format.format(maxMemory / 1024)).append("<br/>");
+    sb.append("total free memory: ").append(format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024)).append("<br/>");
+    return sb.toString();
+  }
+
 }
