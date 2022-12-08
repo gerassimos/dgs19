48e861191182d65eb72179b93f820899f0cf2109
gerassimos <gerassimos@gmail.com>
2022-12-08 21:58:06 +0200

    iot-c-go main

--- resources/iot-c-go/gnmi-server-sim/cmd/main.go
+++ resources/iot-c-go/gnmi-server-sim/cmd/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+var addr string = "0.0.0.0:50051"
+
+//type server struct {
+//	pb.GreetServiceServer
+//}
 
 func main() {
 	fmt.Println("Starting main...")
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	//pb.RegisterGreetServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- resources/iot-c-go/readme.md
+++ resources/iot-c-go/readme.md
@@ -4,4 +4,6 @@
 # cd in to the gnmi-server-sim/gnmi-client-worker  
 go mod init gnmi-server-sim
 go mod init gnmi-client-worker
+
+go get google.golang.org/grpc ## will update hte go.mod file
 ```
