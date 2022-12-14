6166753abdf075e26d895477152d08dc2facebab
gerassimos <gerassimos@gmail.com>
2022-12-14 17:09:16 +0200

    gnmi server go working

--- resources/iot-c-go/gnmi-server-sim/cmd/gnmi_subscribe.go
+++ resources/iot-c-go/gnmi-server-sim/cmd/gnmi_subscribe.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"gnmi/common/proto/gnmi"
 	"io"
 	"log"
@@ -35,7 +36,29 @@ func (*Server) Subscribe(stream gnmi.GNMI_SubscribeServer) error {
 	}
 	log.Printf("Subscribe was invoked with %v\n", subscribeRequest)
 	for {
-		//err = stream.Send(&subscribeRequest)
+		subscriptionList := subscribeRequest.GetSubscribe()
+		subscriptionArray := subscriptionList.GetSubscription()
+		for _, subscription := range subscriptionArray {
+			fmt.Println()
+			subscription.GetPath()
+			update := gnmi.Update{
+				Path: subscription.GetPath(),
+				Val: &gnmi.TypedValue{
+					Value: &gnmi.TypedValue_IntVal{
+						IntVal: 3,
+					},
+				},
+			}
+			log.Printf("update %v\n", update)
+		}
+
+		//response := &gnmi.SubscribeResponse{
+		//	Response: &gnmi.SubscribeResponse_Update{
+		//		Update: update,
+		//	},
+		//}
+		//err = stream.Send(response)
+
 		time.Sleep(3 * time.Second)
 		log.Println("sleeping 3s...")
 		if err != nil {
